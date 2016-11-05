35a56900ecd49b7a5b2fdd51be9265e1ba9aa964
Tian Baoqiang <lovetcn@gmail.com>
2016-11-05 20:15:59 +0800

    update

--- src/main/java/com/fintech/hospital/rssi/RssiMeasure.java
+++ src/main/java/com/fintech/hospital/rssi/RssiMeasure.java
@@ -83,7 +83,7 @@ public class RssiMeasure {
     double distanceSum = positions.stream().mapToDouble(TimedPosition::getRadius).sum();
     double ratioSum = positions.stream().mapToDouble(p->distanceSum-p.getRadius()).sum();
     double[] ratios = positions.stream().mapToDouble(p -> (distanceSum - p.getRadius()) / ratioSum).toArray();
-    TimedPosition start = TimedPosition.mean(positions, ratios);
+    TimedPosition start = TimedPosition.mean(positions, null);
 
     LOG.debug("position evaluation starting from {}", start);
     LOG.debug("targeting {}, ratios: {}", Arrays.toString(prescribedDistance));
